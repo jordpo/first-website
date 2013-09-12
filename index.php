8c9ac2b65b1dc3535ec2dc0e9e248890dc1b469c
Jordan Morano <jordan.morano@gmail.com>
2013-09-11 21:14:11 -0400

    adding index.php file from original hw

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jordan Morano's First Site</title>
+</head>
+<body>
+	<div>
+		<h1><?php echo "Hello World"; ?></h1>
+		<img src="sunflower.JPG" alt="Sunflower and Bee" height="500" width="500">
+	</div>
+	<p>The current server time is <?php echo date("H:i:s a"); ?>.</p>
+	<p>&copy;<?php echo date("Y"); ?> A Jordan Morano Production</p>
+</body>
+</html>
