4b86436fcc876b304c8d2548f2baf24448e61e25
Jordan Morano <jordan.morano@gmail.com>
2013-09-16 19:03:06 -0400

    Added articles, footer, and CSS

--- index.php
+++ index.php
@@ -1,14 +1,87 @@
-<!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Jordan Morano's First Site</title>
+	<title> <?php echo $pageTitle; ?> </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Cinzel+Decorative' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<div>
-		<h1><?php echo "Hello World"; ?></h1>
-		<img src="sunflower.JPG" alt="Sunflower and Bee" height="500" width="500">
+	<?php 
+	$pageTitle = "Jordan Paul's webpage";
+	?>
+	<div class="container">
+			<header>
+				<hgroup>
+				<h1>A complete biography of Jordan Paul Morano</h1>
+				<h2>Venture further to find all of the interesting details.</h2>
+				<nav>
+					<ul>
+						<li><a href="https://www.dropbox.com/s/c0f9pxvrrd3zvy9/JordanMoranoResume.pdf">Resume</a></li>
+						<li><a href="http://about.me/moranojordan">About Me</a></li>
+						<li><a href="https://twitter.com/jordan_morano">Twitter</a></li>
+					</ul>
+				</nav>
+			</header>
+			<section class="main">
+				<article class="his-roots">
+					<header>
+						<h1>His Roots</h1>
+					</header>
+					<div class="image">
+						<img src="img/min_me.jpg" id="me">
+					</div>
+					<p>Jordan Morano was born in Bayshore, Long Island, New York, as the second son of his mom and dad. He has a 
+						total of 8 siblings, which includes 2 full sisters, 2 half-sisters, 2 half-brothers, and 2 step. He moved a total of 16 
+						times growing up, living in New York, New Mexico, New Hampshire, Vermont, and Connecticut. Every summer, 
+						he would go to Fire Island, NY, where his family has an Italian seafood restaurant. His favorite dish, growing
+						up, was shrimp parmesan. And it still is!</p>
+						<aside>
+							<p>His passionas are running, reading, writing, and swimming. He is also learning crossfit.</p>
+						</aside>
+				</article>
+				<article>
+					<header>
+						<h1>Educational Background</h1>
+					</header>
+					<div class="image">
+						<img src="http://huskyraze.webs.com/uconn_logo.jpg" id="school"> 
+					</div> <!-- use an image of UCONN's logo-->
+					<p>He studied at a private school before heading to the University of Connecticut's School of Business. 
+						There he studied Finance and studied abroad in Grenoble, France. He chose Finance because of his enjoyment
+						problem solving and working with numbers. In France, he studied with a very diverse international crowd, 
+						and he improved his French-speaking skills. He fell in love with the culture and its people, eventually
+						leading him back as a professional in future years. He also studied at the Sorbonne University in business
+						in Paris. The highlight of his studies there was doing a week-long seminar in Prague, Czech Republic. In
+						Boston, he recently committed to a path of technology and startups by doing the Startup Institute's Spring
+						2013 program and focusing now 100% on learning front and back-end development.</p>
+					<aside>
+						<p>Currently, he is taking 3 extension school classes, along with a plethora of online ones.</p>
+					</aside>
+				</article>
+				<article>
+					<header>
+						<hgroup>
+							<h1>Professional Experience</h1>
+							<h2>Food business, Entrepreneurship, Operations Analyst</h2>
+						</hgroup>
+					</header>
+					<p>After school, I moved to New York City and worked as the third member of a small team in charge of managing
+						accounts and trades for funds with total assets under management of $6 billion. I worked there for 3 years
+						before moving to Paris, France to work at a French hedge fund that used computer algorithm to direct
+						trades. I worked in the operations there and spoke both French and English during meetings and throughout
+						the day. I also drank a ton of espresso. I left after a year because I wasn't happy with what I did. I 
+						took some MBA classes at the Sorbonne, staying in Paris for an extra year. I returned in 2010 to start
+						my own food business (Italian gelato) with my younger sister in our home town. I did that for a year, then
+						moved to Boston to work in the financial operations of a clean tech company. I finally made the move
+						to focus on the career path of my passion this past Spring: technology and coding!</p>
+					<aside>
+						<p>Each job experience had its merits. I am happy to have had the experiences that I have had.</p>
+					</aside>
+				</article>
+			</section>
+			<footer>
+				<h3>Thanks for Listening!</h3>
+				<p>&copy; <?php echo date("Y"); ?> Jordan Paul Design</p>
+			</footer>
 	</div>
-	<p>The current server time is <?php echo date("H:i:s a"); ?>.</p>
-	<p>&copy;<?php echo date("Y"); ?> A Jordan Morano Production</p>
 </body>
 </html>
